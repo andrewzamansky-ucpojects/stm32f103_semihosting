f402714bc5de4d46a8a7d6393cddb8337c75f8ea
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2019-06-06 15:47:27 -0700

    initial commit

---
+++ app/device_tree/src/device_tree.c
@@ -0,0 +1,11 @@
+#include "_project.h"
+
+#include "dev_management_api.h"
+
+
+/***********************************/
+/********** semihosting_dev ********/
+#define DT_DEV_NAME                      semihosting_dev
+#define DT_DEV_MODULE                    semihosting
+
+#include ADD_CURRENT_DEV
--- app/main/src/main.c
+++ app/main/src/main.c
@@ -12,6 +12,7 @@
 #include "_project_defines.h"
 #include "_project_func_declarations.h"
 
+#include "dev_management_api.h"
 
 
 /***************   defines    *******************/
@@ -24,13 +25,7 @@
 
 /***********   loacal variables    **************/
 
-/******************************************************/
-/**** setup of static devices and device managment ****/
-
-
-
-/**** end ofsetup of static devices and device managment ****/
-/************************************************************/
+static const char hello_world_str[] = "++++++++hello world\r\n";
 
 /*
  * function : main()
@@ -38,6 +33,16 @@
  */
 int main(void)
 {
+	struct dev_desc_t * dev;
+
+	dev = DEV_OPEN("semihosting_dev");
+	if (NULL == dev) goto error;
+	DEV_IOCTL_0_PARAMS(dev, IOCTL_DEVICE_START);
+
+	DEV_WRITE(dev, (uint8_t*)hello_world_str, sizeof(hello_world_str) - 1);
+
+	while(1);
+error :
 	while(1);
 }
 
